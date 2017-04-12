7cbd31671e1176153fab782194fe45f7273ecb82
chancy <penggg37@gmail.com>
2017-04-12 17:47:41 +0800

    modify mysql config

--- common/mysql-config.js
+++ common/mysql-config.js
@@ -1,10 +1,22 @@
 /**
  * Created by Administrator on 2017/3/11.
  */
+/*
 module.exports = {
-    host     : 'hostname',
-    user     : 'username',
-    password : 'passwd',
+    host     : 'localhost',
+    user     : 'root',
+    password : 'sipeng',
     port: '3306',
-    database: 'db'
+    database: 'sp-server'
 };
+server
+*/
+module.exports = {
+    host     : 'bfyyg4hrf-mysql.services.clever-cloud.com',
+    user     : 'unnzoovmsyuprerd',
+    password : '8uMqoWmZwvX9jwPl7sh',
+    port: '3306',
+    database: 'bfyyg4hrf'
+};
+
+
