4ab57e709a4213109ce3e96a8a53dbc278804f78
chancy <penggg37@gmail.com>
2017-04-14 13:05:03 +0800

    RECOVERY

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-//    logger.info(app.get('env') + '==' + err.status);
+    logger.info(app.get('env') + '==' + err.status);
     logger.info("Something went wrong:....", err);
     res.status(err.status || 500);
     res.render('error', {
